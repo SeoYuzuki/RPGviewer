cefbf664c04dc738a57bfcdb9313efdb901fb487
SeoYuzuki <st89200@gmail.com>
2023-07-25 16:57:05 +0800

    [新功能]右鍵分頁可增加視窗

---
+++ src/types/TabContextData.d.ts
@@ -0,0 +1,14 @@
+/**
+ * iview handleContextMenu 專用物件
+ */
+class TabContextData {
+  readonly labelType: string;
+  readonly name: string;
+  readonly closable?: boolean;
+  readonly contextMenu: boolean;
+  readonly disabled: boolean;
+  readonly icon: string;
+  readonly label: string;
+}
+
+export { TabContextData };
